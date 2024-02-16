bab5d5b332f92d98ecac80a9f042151420d1c642
zhufeiyu <1061313113@qq.com>
2024-02-16 20:26:58 +0800

    modify D3Demo

--- src/pages/D3/index.tsx
+++ src/pages/D3/index.tsx
@@ -1,7 +1,7 @@
 import { HeartTwoTone, SmileTwoTone } from '@ant-design/icons';
 import { PageContainer } from '@ant-design/pro-components';
 import { useIntl } from '@umijs/max';
-import { Alert, Card, Typography } from 'antd';
+import { Alert, Button, Card, Typography } from 'antd';
 import React from 'react';
 
 const Admin: React.FC = () => {
@@ -13,6 +13,8 @@ const Admin: React.FC = () => {
         defaultMessage: 'This page can only be viewed by admin',
       })}
     >
+      <Button>查询</Button>
+
       <Card>
         <Alert
           message={intl.formatMessage({
